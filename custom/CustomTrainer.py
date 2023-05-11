6aa33f84c3fbd6478cc00fa73141089b7675013b
olaoot <furin1998@gmail.com>
2023-05-11 06:58:43 +0000

    feat(CustomTrainer): add focal, labelsmoothing, f_with_ls (not tested)

--- custom/CustomTrainer.py
+++ custom/CustomTrainer.py
@@ -3,33 +3,61 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# class FocalLoss(nn.Module):
-#     def __init__(self, weight=None, gamma=2.0, num_classes=30, reduction='mean'):
-#         nn.Module.__init__(self)
-#         self.weight = weight
-#         self.gamma = gamma
-#         self.num_classes = num_classes
-#         self.reduction = reduction
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2.0):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
 
-#     def forward(self, inputs, targets):
-#         log_prob = F.log_softmax(input_tensor, dim=-1)
-#         prob = torch.exp(log_prob)
-#         return F.nll_loss(
-#             ((1 - prob) ** self.gamma) * log_prob,
-#             target_tensor,
-#             weight=self.weight,
-#             reduction=self.reduction
-#         )
+    def forward(self, inputs, targets):
+        log_probs = F.log_softmax(inputs, dim=1)
+        probs = torch.exp(log_probs)
+
+        pt = torch.gather(probs, 1, targets.unsqueeze(1))
+        focal_loss = -self.alpha * (torch.pow(1 - pt, self.gamma)) * log_probs
+        return focal_loss.mean()
+
+class LabelSmoothingLoss(nn.Module):
+    def __init__(self, smoothing=0.0):
+        super(LabelSmoothingLoss, self).__init__()
+        self.smoothing = smoothing
+
+    def forward(self, inputs, targets):
+        num_classes = inputs.size(1)
+        targets = (1 - self.smoothing) * targets + self.smoothing / num_classes
+        log_probs = F.log_softmax(inputs, dim=1)
+        loss = -torch.sum(log_probs * targets, dim=1)
+        return loss.mean()
+    
+class FocalWithLabelSmoothingLoss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2.0, smoothing=0.0):
+        super(FocalWithLabelSmoothingLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.smoothing = smoothing
+        self.focal_loss = FocalLoss(alpha=self.alpha, gamma=self.gamma)
+        self.label_smoothing_loss = LabelSmoothingLoss(smoothing=self.smoothing)
+
+    def forward(self, inputs, targets):
+        focal_loss = self.focal_loss(inputs, targets)
+        label_smoothing_loss = self.label_smoothing_loss(inputs, targets)
+        loss = focal_loss + label_smoothing_loss
+        return loss
 
 
 class CustomTrainer(Trainer):
     def __init__(self, loss_fn=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # self.loss_fn = FocalLoss() if loss_fn == 'focal' else torch.nn.CrossEntropyLoss()
-        self.loss_fn = nn.CrossEntropyLoss()
+        if loss_fn == 'focal':
+            self.loss_fn = FocalLoss()
+        elif loss_fn == 'label_smoothing':
+            self.loss_fn = LabelSmoothingLoss()
+        elif loss_fn == 'focal_with_label_smoothing':
+            self.loss_fn = FocalWithLabelSmoothingLoss()
+        else:
+            self.loss_fn = nn.CrossEntropyLoss()
         
     def compute_loss(self, model, inputs, return_outputs=False):
-        
         outputs = model(**inputs)
         logits = outputs['logits']
         labels = inputs['labels']
